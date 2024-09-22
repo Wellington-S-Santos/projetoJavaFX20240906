40bc455184a47f87195e27c8137f096b7878b327
Wellington-S-Santos <wellington234.az@gmail.com>
2024-09-22 18:20:59 -0300

    Atualização final 20240922

--- AtividadeAvaliativa/src/main/java/etec/sp/gov/br/atividadeavaliativa/HelloController.java
+++ AtividadeAvaliativa/src/main/java/etec/sp/gov/br/atividadeavaliativa/HelloController.java
@@ -17,6 +17,8 @@ public class HelloController {
     @FXML
     private Button btnPesquisar;
     @FXML
+    private ToggleGroup grpTipocontas;
+    @FXML
     private RadioButton rbtCorrente;
     @FXML
     private RadioButton rbtPoupanca;
@@ -35,11 +37,12 @@ public class HelloController {
     @FXML
     private TextField txtPesquisar;
     @FXML
-    private Label lbSaldo;
+    private  Label lbSaldo;
     @FXML
     private TextArea txtAreaDados;
 
     private Conta conta;
+
 
 
     private List<Conta> Contas = new ArrayList<>();
@@ -60,27 +63,46 @@ public class HelloController {
 
     @FXML
     protected void onClickRegistrar() {
+        boolean contaExistente = false;
 
-        if (rbtCorrente.isSelected()) {
-            Conta conta = new Conta(Integer.parseInt(txtNConta.getText()), txtNTitular.getText());
-
-            Contas.add(conta);
-            txtAreaDados.setText(Contas.toString());
-            limparCampos();
-        } else if (rbtEspecial.isSelected()) {
-            Especial especial = new Especial(Integer.parseInt(txtNConta.getText()), txtNTitular.getText(), Double.parseDouble(txtLimite.getText()));
-
-            Contas.add(especial);
-            txtAreaDados.setText(Contas.toString());
-            limparCampos();
-        } else {
-            Poupanca poupanca = new Poupanca(Integer.parseInt(txtNConta.getText()), txtNTitular.getText(), Integer.parseInt(txtDVencimento.getText()));
-            Contas.add(poupanca);
-            txtAreaDados.setText(Contas.toString());
-            limparCampos();
-
+        for (int j = 0; j < Contas.size(); j++) {
+            if (Contas.get(j).getNumero() == Integer.parseInt(txtNConta.getText())) {
+                contaExistente = true;
+                break;
+            }
         }
+        if (!contaExistente){
+            if (rbtCorrente.isSelected()) {
+                Conta conta = new Conta(Integer.parseInt(txtNConta.getText()), txtNTitular.getText());
+                Contas.add(conta);
 
+            } else if (rbtEspecial.isSelected()) {
+                Especial especial = new Especial(Integer.parseInt(txtNConta.getText()), txtNTitular.getText(), Double.parseDouble(txtLimite.getText()));
+                Contas.add(especial);
+
+                } else if (rbtPoupanca.isSelected()) {
+
+                    Poupanca poupanca = new Poupanca(Integer.parseInt(txtNConta.getText()), txtNTitular.getText(), Integer.parseInt(txtDVencimento.getText()));
+                    Contas.add(poupanca);
+
+                }
+
+            txtAreaDados.setText(Contas.toString());
+            limparCampos();
+
+                Alert alertconta = new Alert(Alert.AlertType.INFORMATION);
+                alertconta.setTitle("Conta Registrada!!");
+                alertconta.setHeaderText("Conta realizada com sucesso!!");
+                alertconta.setContentText("Fique avontade para utilizar todos os recursos da plataforma");
+                alertconta.show();
+
+            }else{
+           Alert alertContaExistente = new Alert(Alert.AlertType.ERROR);
+           alertContaExistente.setTitle("Erro");
+           alertContaExistente.setHeaderText("A conta já existe!!");
+           alertContaExistente.setContentText("Não é necessario registrar a conta !!");
+           alertContaExistente.show();
+        }
     }
 
     private void limparCampos() {
@@ -107,12 +129,16 @@ public class HelloController {
 
                 if (rbtCorrente.isSelected() || rbtEspecial.isSelected() || rbtPoupanca.isSelected() ) {
                     conta.depositar(Double.parseDouble(txtValor.getText()));
+                    lbSaldo.setText(String.valueOf(conta.getSaldo()));
+                    limparCampos();
+
+
                 }
-                Alert alertSucesso = new Alert(Alert.AlertType.INFORMATION);
-                alertSucesso.setTitle("Deposito realizado!!!");
-                alertSucesso.setHeaderText("Deposito efeetuado com Sucesso!!");
-                alertSucesso.setContentText("Depositado na conta: " + txtNConta.getText());
-                alertSucesso.show();
+                Alert alertDSucesso = new Alert(Alert.AlertType.INFORMATION);
+                alertDSucesso.setTitle("Deposito realizado!!!");
+                alertDSucesso.setHeaderText("Deposito efetuado com Sucesso!!");
+                alertDSucesso.setContentText("Depositado na conta: " + txtNConta.getText());
+                alertDSucesso.show();
 
                 break;
             }
@@ -129,6 +155,52 @@ public class HelloController {
         }
 
     }
+
+    @FXML
+    protected void onSacarDinheiro (){
+        boolean contaEncontrada = false;
+
+
+        for (int x = 0; x < Contas.size(); x++) {
+            Conta conta = Contas.get(x);
+            if (Integer.parseInt(txtNConta.getText()) == Contas.get(x).getNumero()) {
+                contaEncontrada = true;
+
+                if (rbtCorrente.isSelected() || rbtPoupanca.isSelected() ) {
+                    conta.sacar(Double.parseDouble(txtValor.getText()));
+                    lbSaldo.setText(String.valueOf(conta.getSaldo()));
+                    limparCampos();
+
+
+                } else if (rbtEspecial.isSelected()) {
+                    Especial especial = (Especial) conta;
+                    especial.sacar(Double.parseDouble(txtValor.getText()));
+                    lbSaldo.setText(String.valueOf(conta.getSaldo()));
+                    limparCampos();
+                }
+                Alert alertSSucesso = new Alert(Alert.AlertType.INFORMATION);
+                alertSSucesso.setTitle("Saque realizado!!!");
+                alertSSucesso.setHeaderText("Saque efetuado com Sucesso!!");
+                alertSSucesso.setContentText("Saque da conta: " + txtNConta.getText());
+                alertSSucesso.show();
+
+                break;
+            }
+        }
+
+
+        if (!contaEncontrada) {
+            Alert alertError = new Alert(Alert.AlertType.ERROR);
+            alertError.setTitle("Erro");
+            alertError.setHeaderText("Erro de busca.");
+            alertError.setContentText("O conta não encontrada.");
+            alertError.show();
+            return;
+        }
+
+    }
+
+
 
 
     @FXML
