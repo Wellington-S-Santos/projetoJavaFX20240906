efb5951bc639026faa994ab35960a204c320a780
Wellington-S-Santos <wellington234.az@gmail.com>
2024-11-08 22:38:44 -0300

    20241108 atividade Avaliativa final caixa de dados

---
+++ AtividadeAvaliativaFinal/src/main/java/etec/sp/gov/br/atividadeavaliativafinal/HelloController.java
@@ -0,0 +1,14 @@
+package etec.sp.gov.br.atividadeavaliativafinal;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ AtividadeAvaliativaFinal/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module etec.sp.gov.br.atividadeavaliativafinal {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens etec.sp.gov.br.atividadeavaliativafinal to javafx.fxml;
+    exports etec.sp.gov.br.atividadeavaliativafinal;
+}
